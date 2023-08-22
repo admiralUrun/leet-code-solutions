5b7daf44ed9a8abb163fc9e38787a4d24dd435b1
andrii <admiralurun@gmail.com>
2023-08-22 13:48:31 +0300

    employees_earning_more_than_their_managers.sql

---
+++ sql/employees_earning_more_than_their_managers.sql
@@ -0,0 +1,16 @@
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | salary      | int     |
+-- | managerId   | int     |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+
+
+select e1.name as Employee
+from Employee e1
+         join Employee e2 on e1.managerId = e2.id
+where e1.salary > e2.salary
